c9b008b8d78cedf859e3cc802014bb25e79018e0
Aodhan145 <aodhan.murray@me.com>
2016-12-07 14:19:38 +0000

    Create MCP3301.h

---
+++ Parts/ADCs/MCP3301/MCP3301.h
@@ -0,0 +1,37 @@
+/* 
+* MCP3301.h
+*
+* Created: 06/12/2016 20:59:15
+* Author: Aodhan
+*/
+
+
+#ifndef __MCP3301_H__
+#define __MCP3301_H__
+
+#include <Arduino.h>
+
+class MCP3301
+{
+//variables
+public:
+	double vRef;
+protected:
+private:
+	volatile uint8_t *csPort;
+	uint8_t csPin;
+
+//functions
+public:
+	MCP3301(double vRef, volatile uint8_t *csPort, uint8_t csPin);
+	~MCP3301();
+	int readData(void);
+	double readVoltage(void);
+protected:
+private:
+	MCP3301( const MCP3301 &c );
+	MCP3301& operator=( const MCP3301 &c );
+
+}; //MCP3301
+
+#endif //__MCP3301_H__
